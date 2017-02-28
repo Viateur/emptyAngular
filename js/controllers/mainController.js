1cecc76915453d1fa387030e160970d55a47aaa5
Viateur <viateur.ayegnon@gmail.com>
2017-02-28 16:28:16 +0100

    solution

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,12 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+
+
+function ajouterTableau(){
+  let enfantRow = document.createElement('TR');
+  enfantRow.innerHTML=`<td>${$scope.lastname}</td><td>${$scope.firstname}</td><td>${$scope.email}</td>`
+  let parentTb = document.getElementById("parentTable");
+  parentTb.appendChild(enfantRow);
+  }
+document.getElementById('ajouter').addEventListener('click', ajouterTableau);
 }
